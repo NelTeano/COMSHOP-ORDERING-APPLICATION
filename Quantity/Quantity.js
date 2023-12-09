0b639294a0591caa123a6c2165157e1cd482151a
panganibaneli <eliel.panganiban@gmail.com>
2023-12-10 04:52:20 +0800

    sad

--- Quantity/Quantity.js
+++ Quantity/Quantity.js
@@ -1,27 +1,28 @@
-<script>
-  document.addEventListener('DOMContentLoaded', function () {
-    const decrementButton = document.querySelector('.decrement');
-    const incrementButton = document.querySelector('.increment');
-    const quantityInput = document.querySelector('.quantity-input');
+document.addEventListener('DOMContentLoaded', function () {
+  const decrementButton = document.querySelector('.decrement');
+  const incrementButton = document.querySelector('.increment');
+  const quantityInput = document.querySelector('.quantity-input');
 
-    decrementButton.addEventListener('click', function () {
-      let currentQuantity = parseInt(quantityInput.value, 10);
+  decrementButton.addEventListener('click', decrementQuantity);
+  incrementButton.addEventListener('click', incrementQuantity);
 
-      if (currentQuantity > 1) {
-        currentQuantity -= 1;
-        quantityInput.value = currentQuantity;
-      }
-    });
+  function decrementQuantity() {
+    let currentQuantity = parseInt(quantityInput.value, 10);
 
-    incrementButton.addEventListener('click', function () {
-      let currentQuantity = parseInt(quantityInput.value, 10);
-      currentQuantity += 1;
+    if (currentQuantity > 1) {
+      currentQuantity -= 1;
       quantityInput.value = currentQuantity;
-    });
-  });
-
-  function addToOrders() {
-    // Add your logic for adding to orders here
-    alert('Added to Orders: ' + document.querySelector('.quantity-input').value);
+    }
   }
-</script>
+
+  function incrementQuantity() {
+    let currentQuantity = parseInt(quantityInput.value, 10);
+    currentQuantity += 1;
+    quantityInput.value = currentQuantity;
+  }
+});
+
+function addToOrders() {
+  // Add your logic for adding to orders here
+  alert('Added to Orders: ' + document.querySelector('.quantity-input').value);
+}
