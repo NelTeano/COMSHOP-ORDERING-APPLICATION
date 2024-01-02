3485b4a0568a1ebc5066e11e21c0ae69c230656a
denzelsanchez <153376811+denzelsanchez@users.noreply.github.com>
2024-01-03 02:00:24 +0800

    Pesos prices

--- Order Cart/script.js
+++ Order Cart/script.js
@@ -76,7 +76,7 @@ closeCart.addEventListener('click', () => {
                 newProduct.innerHTML = 
                 `<img src="${product.image}" alt="">
                 <h2>${product.name}</h2>
-                <div class="price">$${product.price}</div>
+                <div class="price">₱${product.price}</div>
                 <button class="addCart">Add To Cart</button>`;
                 listProductHTML.appendChild(newProduct);
                 
